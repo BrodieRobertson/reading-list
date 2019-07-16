947be9498f61698500b76246a87ff6dd2486e093
Brodie Robertson <brodierobertson54321@gmail.com>
2019-07-16 10:03:40 +0930

    Add basic template for the book list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,24 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     BookListComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    EditBookComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: BookListComponent },
-      { path: 'book/:bookId', component: BookDetailsComponent }
+      { path: 'book/:bookId', component: BookDetailsComponent },
+      { path: 'book/new', component: EditBookComponent },
+      { path: 'book/:bookId/edit', component: EditBookComponent }
     ])
   ],
   providers: [],
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -12,47 +12,57 @@ import { Book } from '../models/book';
 export class BookListComponent implements OnInit {
   bookList: Array<Book>
   currentList: Lists;
+  listName: String;
   Lists = Lists;
   
   constructor(private books: BookService) {}
 
   ngOnInit() {
-    this.currentList = Lists.CURRENTLY_READING;
-    this.getList(this.currentList);
+    this.currentList = Lists.ALL;
+    this.loadList(this.currentList);
   }
 
   /**
-   * Gets a list of books
+   * Loads a list of books
    * 
    * @param list The list to load
    */
-  getList(list: Lists) {
+  loadList(list: Lists) {
     this.bookList = this.books.getBooks();
     switch(list) {
       case Lists.CURRENTLY_READING:
+        this.listName = "Currently Reading Books"
         return this.bookList.filter(book => {
           return book.currentlyReading;
         })
       case Lists.DROPPED:
+        this.listName = "Dropped Books"
         return this.bookList.filter(book => {
           return book.dropped;
         })
       case Lists.NOT_OWNED:
+        this.listName = "Not Owned Books"
         return this.bookList.filter(book => {
           return book.notOwned;
         })
       case Lists.OWNED:
+        this.listName = "Owned Books"
         return this.bookList.filter(book => {
           return book.owned;
         })
       case Lists.READ:
+        this.listName = "Read Books"
         return this.bookList.filter(book => {
           return book.read;
         })
       case Lists.UNREAD:
+        this.listName = "Unread Books"
         return this.bookList.filter(book => {
           return book.unread;
         })
+      default:
+        this.listName = "All Books"
+        return this.bookList;
     }
   }
 }
