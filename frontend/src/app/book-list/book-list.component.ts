0236ae786850b02d7ef0727606b6e7e895241cf8
Brodie Robertson <brodierobertson54321@gmail.com>
2019-08-24 15:18:29 +0930

    Highlight the selected list button

--- frontend/src/app/book-list/book-list.component.ts
+++ frontend/src/app/book-list/book-list.component.ts
@@ -11,7 +11,10 @@ import { ReadingState } from '../reading-state.enum';
 })
 
 export class BookListComponent implements OnInit {
-  bookList: Array<Book>
+  bookList: Array<Book>;
+  buttonStyles: Array<string>;
+  previousList: number;
+  selectedList: number;
   currentList: Lists;
   listName: String;
   Lists = Lists;
@@ -20,6 +23,12 @@ export class BookListComponent implements OnInit {
 
   ngOnInit() {
     this.currentList = Lists.ALL;
+    this.previousList = -1;
+    this.selectedList = -1;
+    this.buttonStyles = [];
+    for(var i = 0; i < 8; ++i) {
+      this.buttonStyles.push("regular-button")
+    }
     this.loadList(this.currentList);
   }
 
@@ -30,52 +39,74 @@ export class BookListComponent implements OnInit {
    */
   loadList(list: Lists) {
     this.bookList = this.books.getBooks();
+    if(this.selectedList != -1) {
+      this.previousList = this.selectedList;
+    }
+
     switch(list) {
       case Lists.CURRENTLY_READING:
         this.listName = "Currently Reading Books"
+        this.selectedList = 1;
         this.bookList = this.bookList.filter(book => {
           return book.readingState === ReadingState.CURRENTLY_READING;
         })
         break;
-      case Lists.DROPPED:
-        this.listName = "Dropped Books"
-        this.bookList = this.bookList.filter(book => {
-          return book.readingState === ReadingState.DROPPED;
-        })
-        break;
       case Lists.PLAN_TO_READ:
         this.listName = "Plan to Read Books"
+        this.selectedList = 2;
         this.bookList = this.bookList.filter(book => {
           return book.readingState === ReadingState.PLAN_TO_READ;
         })
         break;
-      case Lists.NOT_OWNED:
-        this.listName = "Not Owned Books"
-        this.bookList = this.bookList.filter(book => {
-          return !book.owned;
-        })
-        break;
-      case Lists.OWNED:
-        this.listName = "Owned Books"
-        this.bookList = this.bookList.filter(book => {
-          return book.owned;
-        })
-        break;
       case Lists.READ:
         this.listName = "Read Books"
+        this.selectedList = 3;
         this.bookList = this.bookList.filter(book => {
           return book.read;
         })
         break;
       case Lists.UNREAD:
         this.listName = "Unread Books"
+        this.selectedList = 4;
         this.bookList = this.bookList.filter(book => {
           return !book.read;
         })
         break;
+      case Lists.OWNED:
+        this.listName = "Owned Books"
+        this.selectedList = 5;
+        this.bookList = this.bookList.filter(book => {
+          return book.owned;
+        })
+        break;
+      case Lists.NOT_OWNED:
+        this.listName = "Not Owned Books"
+        this.selectedList = 6;
+        this.bookList = this.bookList.filter(book => {
+          return !book.owned;
+        })
+        break;
+      case Lists.DROPPED:
+        this.listName = "Dropped Books"
+        this.selectedList = 7;
+        this.bookList = this.bookList.filter(book => {
+          return book.readingState === ReadingState.DROPPED;
+        })
+        break;
       default:
         this.listName = "All Books"
+        this.selectedList = 0;
         break;
     }
+
+    this.updateButtonStyles()
+  }
+
+  updateButtonStyles() {
+    if(this.previousList != -1) {
+      this.buttonStyles[this.previousList] = "regular-button"
+    }
+    this.buttonStyles[this.selectedList] = "regular-button-dark"
   }
 }
+
